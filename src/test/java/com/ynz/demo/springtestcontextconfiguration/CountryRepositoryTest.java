18c3fced93efaafc8c2f89285d1670bb07e8b082
Yichun Zhao <zhaoyichun@hotmail.com>
2021-07-18 12:08:59 +0200

    Using @ContextConfiguration to re-config ApplicationContext in a runtime

--- src/test/java/com/ynz/demo/springtestcontextconfiguration/CountryRepositoryTest.java
+++ src/test/java/com/ynz/demo/springtestcontextconfiguration/CountryRepositoryTest.java
@@ -1,11 +1,20 @@
 package com.ynz.demo.springtestcontextconfiguration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +22,34 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+@ContextConfiguration(initializers = {CountryRepositoryTest.TestResources.class})
+@Slf4j
 class CountryRepositoryTest {
+
+    @Container
+    private static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13")
+            .withDatabaseName("contextconfig")
+            .withUsername("postgres")
+            .withPassword("test");
+
+    static class TestResources implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(applicationContext.getEnvironment());
+        }
+    }
+
     @Autowired
     private CountryRepository repository;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     void shouldHavingMoreThan60Countries() {
@@ -24,6 +58,11 @@ class CountryRepositoryTest {
         countries.forEach(countryList::add);
 
         assertThat(countryList, Matchers.hasSize(60));
+    }
+
+    @Test
+    void shouldDataSourceAutoConfigured() throws Exception {
+        assertThat(dataSource, Matchers.notNullValue());
     }
 
 }
